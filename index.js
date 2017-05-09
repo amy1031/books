bbceec4f7775dd1908bb52af10d6cd23a9625187
Amy Roche <amy.roche31@gmail.com>
2017-05-09 14:01:23 -0600

    get, post, put, and delete books to database

--- index.js
+++ index.js
@@ -3,80 +3,111 @@ var bodyParser = require('body-parser')
 var server = express()
 var port = 9001
 
+
+// !! Database Stuff
+var mongoose = require('mongoose')
+var connectionString = 'mongodb://student:student@ds029107.mlab.com:29107/books'
+var connection = mongoose.connection
+
+//Does not change
+mongoose.connect(connectionString, {
+    server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+    replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }
+});
+
+connection.on('error', function (err) {
+    console.log('There was a connection problem', err)
+})
+
+connection.once('open', function() {
+    console.log('We are now connected to the books database')
+    server.listen(port, function() {
+        console.log('Yep, it is working', 'http://localhost:' + port)
+    })
+})
+
+// !!End database stuff
+
+
+//Parses the request data into jason, gives access to req.body
 server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: true }))
+
 
 server.get('/', function(req, res, next) {
     res.send(200, '<h1>Are you looking for <a href="/books">Books</a>?</h1>')
 })
 
-var books = [{
-    title: "Catch Me If You Can",
-    published: 1980,
-    rating: 4.5,
-    author: "Frank Abignale"
-}, {
-    title: "Split Second",
-    published: 2015,
-    rating: 4.5,
-    author: "Douglas E. Richards"
-}]
+// BOOK in the DB (can create multiple schemas)
+var Schema = mongoose.Schema
+var BookSchema = new Schema({
+    title: {type: String, required: true},
+    published: {type: Number, required: true},
+    rating: {type: Number, required: true},
+    author: {type: String, required: true}
+})
+//{type: Boolean, required: true, default: false}
+
+//In the database, you'll have a collection of 'Book' which will follow the Book schema model
+var Book = mongoose.model('Book', BookSchema)
+// END BOOK
+
 
 server.get('/books', function(req, res, next) {
-    res.send(books)
+    Book.find({}).then(function(book) {
+        res.send(book)
+    })
 })
+
+server.get('/books/search', function(req, res, next){
+    var query = req.query
+    Book.find({}).then(function (books) {
+        res.send(books)
+    })
+})
+
 
 server.get('/books/:id', function(req, res, next) {
     var id = req.params.id
 
-    if(books[id]) {
-        res.send(books[id])
-    } else {
-        res.send(404, {
-            error: {
-                message: "Sorry, no book at id " + id
-            }
-        })
-    }
+    Book.findById(id).then(function(book) {
+        res.send(book)
+    }).catch(function(e) {
+        res.send(e)
+    })
 })
 
-server.post('/books', function(req, res, next) {
-    var newBook = req.body
-
-    if(newBook.title && newBook.published && newBook.rating && newBook.author) {
-        books.push(newBook)
-        res.send('Book added')
-    } else {
-        res.send (401, "Sorry, you must include a title, year published, rating, and author")
-    }
+server.post('/books', function (req, res, next) {
+  var newBook = req.body
+  Book.create(newBook)
+    .then(function (newlyCreatedBook) {
+      res.send(newlyCreatedBook)
+    })
 })
+
 
 server.put('/books/:id', function(req, res, next){
     var id = req.params.id
-
-    if (books[id]) {
-        var book = books[id]
-        book.title = req.body.title || book.title
-        book.published = req.body.published || book.published
-        book.rating = req.body.rating || book.rating
-        book.author = req.body.author || book.author
-        res.send(book)
-    } else {
-        res.send (404, "Sorry, no book at that id")
-    }
+    var updatedBook = req.body
+    Book.findByIdAndUpdate(id, updatedBook).then(function() {
+        res.send(updatedBook)
+    })
 }) 
+
 
 server.delete('/books/:id', function(req, res, next) {
     var id = req.params.id
 
-    if(books[id]){
+
+    Book.findByIdAndRemove(id).then(function() {
+        res.send('Book Removed')
+    })
+ })
+
+   /* if(books[id]){
         var book = books[id]
         books.splice(book, 1)
         res.send(books)
     } else {
         res.send(404, "Sorry, no book at that id")
-    }
-})
-
-server.listen(port, function() {
-    console.log("This server is working and listening for requests on port: ", port)
-})
+    }*/
